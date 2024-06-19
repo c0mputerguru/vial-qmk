5ceb2e8335faf0552217fdb15a3e7c2d35eadf63
Anand Patel <anand.b.patel@gmail.com>
2024-06-19 15:38:12 -0700

    Add config for autoshift and caps word.

--- keyboards/dcpedit/pillzmod/keymaps/c0mputerguru/config.h
+++ keyboards/dcpedit/pillzmod/keymaps/c0mputerguru/config.h
@@ -2,3 +2,8 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 #pragma once
 #define VIAL_KEYBOARD_UID {0xFC, 0xAF, 0x9D, 0xAD, 0x8C, 0x2E, 0x02, 0x85}
+
+#define TAPPING_TERM 200
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+#define RETRO_SHIFT 500
+#define AUTO_SHIFT_TIMEOUT 170 //determined by auto shift setup
