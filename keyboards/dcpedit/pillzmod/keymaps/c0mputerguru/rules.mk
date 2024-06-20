f2d68471ca2fe6b05af55de15896109394bb4928
Anand Patel <anand.b.patel@gmail.com>
2024-06-19 18:40:03 -0700

    Enable NKRO and disable QMK settings in vial to allow retro-shift.

--- keyboards/dcpedit/pillzmod/keymaps/c0mputerguru/rules.mk
+++ keyboards/dcpedit/pillzmod/keymaps/c0mputerguru/rules.mk
@@ -5,5 +5,8 @@ VIA_ENABLE = yes
 VIAL_ENABLE = yes
 VIAL_INSECURE = yes
 
+# Disable QMK settings in Vial to support retro shift
+QMK_SETTINGS = no
+
 SRC += features/layer_lock.c
 SRC += features/select_word.c
